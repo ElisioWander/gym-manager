1111d8e19aaa25c1f4b638e5401d2325fb1afc15
Elisio Wander <elisio741@hotmail.com>
2021-02-26 23:27:00 -0300

    gym-manager

--- src/app/controllers/InstructorsController.js
+++ src/app/controllers/InstructorsController.js
@@ -16,6 +16,7 @@ module.exports = {
             limit,
             offset
         }
+        
 
         let results = await InstructorModel.paginate(params)
         const instructors = results.rows
