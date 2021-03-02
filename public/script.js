7ea838c94432866fd8ca2294dfe0e15e50637092
Elisio Wander <elisio741@hotmail.com>
2021-03-01 22:10:57 -0300

    gym-manager-pagination

--- public/script.js
+++ public/script.js
@@ -7,3 +7,45 @@ for(item of menuItems) {
         item.classList.add('active')
     }
 }
+
+let totalPages = 20,
+    selectedPage = 15,
+    oldPage,
+    pages = []
+
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+        
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if(firstAndLastPage || pagesAfterSelectedPage && pagesBeforeSelectedPage) {
+            if(oldPage && currentPage - oldPage > 2) {
+                pages.push("...")
+            }
+
+            if(oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
+        }
+
+    }
+
+    const pagination = document.querySelector(".pagination")
+
+    let elements = ""
+
+    for(let page of pages) {
+        if(String(page).includes("...")) {
+            elements += `<span>${page}</span>`
+        } else {
+            elements += `<a href="?pages=${page}">${page}</a>`
+        }
+
+    }
+
+    pagination.innerHTML = elements
